6e3219227174e1789ca15b9dd948472948ec5b78
axreldable <aleksei.starikov.ax@gmail.com>
2023-11-22 10:36:34 +0400

    c_prog - Add stepik_c++_prog

---
+++ stepik_c++_prog/compile_example/main.cpp
@@ -0,0 +1,11 @@
+// #include <iostream>
+
+#include "square.hpp"
+
+int main() {
+    // int x = square(5);
+    square(5);
+
+    // std::cout << x;
+    return 0;
+}
---
+++ stepik_c++_prog/compile_example/square.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Aleksei Starikov on 22.11.2023.
+//
+
+#ifndef SQUARE_H
+#define SQUARE_H
+
+
+
+int square(int x);
+
+
+
+#endif //SQUARE_H
