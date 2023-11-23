92c799c97b93ca9ef72d6d223ed2287cd2bef99b
axreldable <aleksei.starikov.ax@gmail.com>
2023-11-23 21:50:59 +0400

    c_prog - Add some tasks

--- stepik_c++_prog/tasks/funcs.cpp
+++ stepik_c++_prog/tasks/funcs.cpp
@@ -15,3 +15,27 @@ void max_func(int a, int b, int* c) {
 }
 
 #define MAX(x, y, r) {int a_ = (x); int b_ = (y); int c_ = (a_ > b_ ? a_ : b_); r = c_;}
+
+int log(int x) {
+    int p = 0;
+    int rez = 1;
+    while (rez <= x) {
+        rez = rez * 2;
+        p += 1;
+    }
+    return p - 1;
+}
+
+unsigned gcd(unsigned a, unsigned b)
+{
+    if (a == 0) {
+        return b;
+    }
+    if (b == 0) {
+        return a;
+    }
+    if (b > a) {
+        return gcd(a, b % a);
+    }
+    return gcd(a % b, b);
+}
--- stepik_c++_prog/tasks/main.cpp
+++ stepik_c++_prog/tasks/main.cpp
@@ -1,21 +1,22 @@
-#include <cmath>
 #include <iostream>
 
 using namespace std;
 
-int main() {
-    int a, b, c, d;
-    cin >> a >> b >> c;
-
-    d = b * b - 4 * a * c;
-    if (d < 0) {
-        cout << "No real roots\n";
-        return 0;
+void call(int x) {
+    int xx;
+    cin >> xx;
+    if (xx == 0) {
+        cout << x << ' ';
+        return;
     }
+    call(xx);
+    cout << x << ' ';
+}
 
-    double x1 = (-b - sqrt(d)) / (2 * a);
-    double x2 = (-b + sqrt(d)) / (2 * a);
-
-    cout << x1 << " " << x2 << "\n";
+int main()
+{
+    int x;
+    cin >> x;
+    call(x);
     return 0;
 }
