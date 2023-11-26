92283f50d219001f18031e56f22c515b950fa74a
axreldable <aleksei.starikov.ax@gmail.com>
2023-11-26 22:05:44 +0400

    c_prog - Add some tasks

--- stepik_c++_prog/tasks/main.cpp
+++ stepik_c++_prog/tasks/main.cpp
@@ -9,10 +9,26 @@ void print_arr(int a[], unsigned size) {
     cout << '\n';
 }
 
+void reverse(int a[], unsigned start, unsigned end) {
+    while (start < end) {
+        int t = a[start];
+        a[start++] = a[end];
+        a[end--] = t;
+    }
+}
 
-int main()
-{
-    int x[10] = {1, 2, 3, 4, 5};
-    print_arr(x, 10);
+void rotate(int a[], unsigned size, int shift) {
+    reverse(a, 0, size - 1);
+    reverse(a, 0, size - (shift % size) - 1);
+    reverse(a, size - (shift % size), size - 1);
+}
+
+
+int main() {
+    int x[5] = {1, 2, 3, 4, 5};
+
+    print_arr(x, 5);
+    rotate(x, 5, 2);
+    print_arr(x, 5);
     return 0;
 }
