7f8ae8e2f2c28b951d06976a2d901a925cd27b26
axreldable <aleksei.starikov.ax@gmail.com>
2023-11-30 10:13:03 +0400

    c_prog - Add some tasks

--- stepik_c++_prog/main.cpp
+++ stepik_c++_prog/main.cpp
@@ -2,66 +2,26 @@
 
 using namespace std;
 
-void print_arr(int a[], unsigned size) {
-    for (int i = 0; i < size; ++i) {
-        cout << a[i] << ' ';
-    }
-    cout << '\n';
+void swap(int* a, int* b) {
+    const int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-unsigned strlen_1(const char* str) {
-    unsigned r = 0;
-    while (*str != '\0') {
-        str += 1;
-        r += 1;
-    }
-    return r;
-}
-
-void strcat_1(char* to, const char* from) {
-    unsigned a = strlen_1(to);
-    unsigned b = strlen_1(from);
-    for (int i = 0; i < b; ++i) {
-        to[a + i] = from[i];
-    }
-    to[a + b] = '\0';
-}
-
-bool is_the_same(const char* source, const char* pattern) {
-    unsigned a = strlen_1(source);
-    unsigned b = strlen_1(pattern);
-    if (a < b) {
-        return false;
-    }
-    for (int i = 0; i < b; ++i) {
-        if (source[i] != pattern[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int strstr_1(const char* text, const char* pattern) {
-    unsigned b = strlen_1(pattern);
-    if (b == 0) {
-        return 0;
-    }
-    unsigned a = strlen_1(text);
-    for (int i = 0; i < a; ++i) {
-        if (text[i] == pattern[0]) {
-            if (is_the_same(&text[i], pattern)) {
-                return i;
-            }
-        }
-    }
-    return -1;
+void swap_link(int& a, int& b) {
+    const int t = a;
+    a = b;
+    b = t;
 }
 
 int main() {
-    char first[] = "123456123";
-    char second[] = "456";
-    cout << is_the_same(first, second) << "\n";
-    cout << is_the_same("456", second) << "\n";
-    cout << strstr_1(first, second) << "\n";
+    int a = 1;
+    int b = 2;
+
+    cout << a << ' ' << b << '\n';
+    swap(&a, &b);
+    cout << a << ' ' << b << '\n';
+    swap_link(a, b);
+    cout << a << ' ' << b << '\n';
     return 0;
 }
--- stepik_c++_prog/tasks/funcs.cpp
+++ stepik_c++_prog/tasks/funcs.cpp
@@ -131,3 +131,58 @@ bool max(int *p, int *q, int **res) {
     }
     return true;
 }
+
+void print_arr(int a[], unsigned size) {
+    for (int i = 0; i < size; ++i) {
+        cout << a[i] << ' ';
+    }
+    cout << '\n';
+}
+
+unsigned strlen_1(const char* str) {
+    unsigned r = 0;
+    while (*str != '\0') {
+        str += 1;
+        r += 1;
+    }
+    return r;
+}
+
+void strcat_1(char* to, const char* from) {
+    unsigned a = strlen_1(to);
+    unsigned b = strlen_1(from);
+    for (int i = 0; i < b; ++i) {
+        to[a + i] = from[i];
+    }
+    to[a + b] = '\0';
+}
+
+bool is_the_same(const char* source, const char* pattern) {
+    unsigned a = strlen_1(source);
+    unsigned b = strlen_1(pattern);
+    if (a < b) {
+        return false;
+    }
+    for (int i = 0; i < b; ++i) {
+        if (source[i] != pattern[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int strstr_1(const char* text, const char* pattern) {
+    unsigned b = strlen_1(pattern);
+    if (b == 0) {
+        return 0;
+    }
+    unsigned a = strlen_1(text);
+    for (int i = 0; i < a; ++i) {
+        if (text[i] == pattern[0]) {
+            if (is_the_same(&text[i], pattern)) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
